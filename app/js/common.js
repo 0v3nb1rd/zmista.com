9e8aecd9763dce85dbc708b61369ff92bf48ef5b
0v3nb1rd <0v3nb1rd@gmail.com>
2021-01-22 18:51:34 +0200

    add header

---
+++ app/js/common.js
@@ -0,0 +1,13 @@
+$(function () {
+  // Custom JS
+
+  var hashLinks = document.querySelectorAll("a[href^='#']");
+  [].forEach.call(hashLinks, function (link) {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
+      history.pushState({}, '', link.href);
+      history.pushState({}, '', link.href);
+      history.back();
+    });
+  });
+});
