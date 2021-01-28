d36aab090f8aaf7582e64ccb681bfac67b6f4795
0v3nb1rd <0v3nb1rd@gmail.com>
2021-01-29 01:49:58 +0200

    add modal js

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,14 @@
 $(function () {
   // Custom JS
+  $('#setReview').click(function (e) {
+    e.preventDefault();
+    $('body').addClass('no-scroll');
+    $('.modal-review').addClass('active');
+  });
+  $('.close-btn').click(function () {
+    $('body').removeClass('no-scroll');
+    $('.modal-review').removeClass('active');
+  });
 
   var hashLinks = document.querySelectorAll("a[href^='#']");
   [].forEach.call(hashLinks, function (link) {
